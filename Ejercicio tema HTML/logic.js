9f5e18783d4c999da09fa04706b1b4d8f783af77
Alonsde7 <alvaro20snbr@gmail.com>
2023-04-06 18:27:22 +0200

    Resolucion de un error, hay que mirar los errores

---
+++ Ejercicio tema HTML/logic.js
@@ -0,0 +1,14 @@
+function onLoad(event){
+    sessionStorage.setItem("x","x");
+    sessionStorage.setItem("y","y");
+    sessionStorage.setItem("z","z");
+    console.log("hola");
+}
+
+function onSubmit(evt, form){
+
+
+
+
+    
+}
