1e85fc73aaa772c49f2cbf0f48902a1b6308e454
Alonsde7 <alvaro20snbr@gmail.com>
2023-04-07 01:20:51 +0200

    Creado botones de crear cosas

--- Ejercicio tema HTML/logic.js
+++ Ejercicio tema HTML/logic.js
@@ -12,17 +12,38 @@ function deleteItem(element) {
     const padre = element.padre;
 }
 
+function newTech() {
+
+}
+
+function newAuthor() {
+
+}
+
+function newComp() {
+
+}
+
 function onLoging() {
 
     if (localStorage.getItem(document.getElementById("user_form").value) == document.getElementById("password_form").value) {
 
         const formulario = document.getElementById("logging_form");
+        formulario.innerHTML = "<input type=\"submit\" name=\"logout\" value=\"logout\">";
+
         const table = document.getElementById("principal_table");
 
-        formulario.innerHTML = "<input type=\"submit\" name=\"logout\" value=\"logout\">";
         for (let elemento of table.getElementsByTagName("td")) {
             elemento.innerHTML += "<input type=\"button\" name=\"edit\" value=\"edit\" onclick=\"editItem(this)\">";
-            elemento.innerHTML += "<input type=\"button\" name=\"delete\" value=\"delete\" onclick=\deleteItem(this)\">";
+            elemento.innerHTML += "<input type=\"button\" name=\"delete\" value=\"delete\" onclick=\"   deleteItem(this)\">";
         }
+
+        const newRow = document.createElement("tr");
+        table.appendChild(newRow);
+
+        newRow.innerHTML += "<td><form><input type=\"button\" name=\"nueva tecnolog&iacute;a\" value=\"nueva tacnolog&iacute;a\" onclick=\"newTech()\"></form></td>"
+        newRow.innerHTML += "<td><form><input type=\"button\" name=\"nuevo autor\" value=\"nuevo autor\" onclick=\"newAuthor()\"></form></td>"
+        newRow.innerHTML += "<td><form><input type=\"button\" name=\"nueva empresa\" value=\"nueva empresa\" onclick=\"newComp()\"></form></td>"
+
     }
 }
