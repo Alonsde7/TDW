a8e67513a2a7f0efc91e46d278947402b8c5f12a
Alonsde7 <alvaro20snbr@gmail.com>
2023-04-07 00:14:35 +0200

    Cambios en logic.js

--- Ejercicio tema HTML/logic.js
+++ Ejercicio tema HTML/logic.js
@@ -1,14 +1,28 @@
-function onLoad(event){
-    sessionStorage.setItem("x","x");
-    sessionStorage.setItem("y","y");
-    sessionStorage.setItem("z","z");
-    console.log("hola");
+function onLoad(event) {
+    localStorage.setItem("x", "x");
+    localStorage.setItem("y", "y");
+    localStorage.setItem("z", "z");
 }
 
-function onSubmit(evt, form){
+function editItem(element) {
+    const padre = element.padre;
+}
 
+function deleteItem(element) {
+    const padre = element.padre;
+}
 
+function onLoging() {
 
+    if (localStorage.getItem(document.getElementById("user_form").value) == document.getElementById("password_form").value) {
 
-    
+        const formulario = document.getElementById("logging_form");
+        const table = document.getElementById("principal_table");
+
+        formulario.innerHTML = "<input type=\"submit\" name=\"logout\" value=\"logout\">";
+        for (let elemento of table.getElementsByTagName("td")) {
+            elemento.innerHTML += "<input type=\"button\" name=\"edit\" value=\"edit\" onclick=\"editItem(this)\">";
+            elemento.innerHTML += "<input type=\"button\" name=\"delete\" value=\"delete\" onclick=\deleteItem(this)\">";
+        }
+    }
 }
